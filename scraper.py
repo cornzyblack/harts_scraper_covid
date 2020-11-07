a77978415aa204fa1f11929741ac9653bb2a3c2a
Adah Eni <eniwokepdm@gmail.com>
2020-11-07 19:21:16 +0100

    refactor: simplify scraper by using  helper methods

--- scraper.py
+++ scraper.py
@@ -1,28 +1,5 @@
-from helper import get_harts_html
+from helper import extract_table
 
 url = "https://www.herts.ac.uk/coronavirus/covid-19-case-tracker"
-
-result = get_harts_html(url)
-
-soup = BeautifulSoup(result)
-
-# soup.find("<h2>Latest University of Hertfordshire COVID-19 positive test results</h2>")
-latest_link = soup.find("h2")
-
-covid_details_list = list(latest_link.next_siblings)
-
-start_date = covid_details_list[0]
-covid_table = covid_details_list[1]
-
-df = pd.read_html(str(covid_table))[0]
-df = df.rename(columns={"Unnamed: 0": "days"})
-
-# datetime.now(tz="London")
-import  pytz
-british_tz = pytz.timezone("GMT")
-time_now = datetime.now(tz=british_tz)
-current_year = time_now.year
-
-start_datetime = dateparser.parse(start_date.text.split('Week commencing')[-1] + str(current_year))
-
-df["dates"] = pd.date_range(start_datetime.strftime("%m/%d/%Y"), periods=df.shape[0])
+result = extract_table(url)
+print(result)
