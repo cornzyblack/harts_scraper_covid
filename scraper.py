eb9e9f74898bc2d77d1b829343d04ad0d37363c8
Adah Eni <eniwokepdm@gmail.com>
2020-11-08 22:26:18 +0100

    feat: fix issue with empty DF

--- scraper.py
+++ scraper.py
@@ -1,5 +1,8 @@
 from helper import extract_table
+import pandas as pd
 
 url = "https://www.herts.ac.uk/coronavirus/covid-19-case-tracker"
 result = extract_table(url)
-print(result)
+
+if isinstance(result, pd.DataFrame):
+    print(result.to_json(orient="records"))
