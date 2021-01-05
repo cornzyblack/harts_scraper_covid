39c2344a25991ae82b6310532a8347d053855e10
Eniwoke Cornelius <eniwokepdm@gmail.com>
2021-01-05 16:08:09 +0100

    feat: make function follow Google style guide

--- helper.py
+++ helper.py
@@ -6,10 +6,20 @@ import re
 from datetime import datetime
 from typing import Optional
 import dateparser
-import  pytz
+import pytz
 
 
-def get_harts_html(url: str) -> Optional[str] :
+def get_harts_html(url: str) -> Optional[str]:
+    """
+    Returns the HTML content of the URL.
+
+    Parameters:
+        url (str):The Harts Covid URL page
+
+    Returns:
+        str: The page in HTML format
+    """
+
     result = None
     try:
         response = requests.get(url)
@@ -37,11 +47,16 @@ def extract_table(url: str) -> Optional[pd.DataFrame]:
         time_now = datetime.now(tz=british_tz)
         current_year = time_now.year
 
-        start_datetime = dateparser.parse(start_date.text.split('Week commencing')[-1] + str(current_year))
+        start_datetime = dateparser.parse(
+            start_date.text.split("Week commencing")[-1] + str(current_year)
+        )
 
-        df["dates"] = pd.date_range(start_datetime.strftime("%m/%d/%Y"), periods=df.shape[0])
+        df["dates"] = pd.date_range(
+            start_datetime.strftime("%m/%d/%Y"), periods=df.shape[0]
+        )
 
     return df
+
 
 def upload_db():
     pass
