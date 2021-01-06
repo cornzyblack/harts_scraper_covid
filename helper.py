bf1b9a7464ed681c66f424d8866bb965bc757a71
Eniwoke Cornelius <eniwokepdm@gmail.com>
2021-01-06 15:08:45 +0100

    fix: fix issue with not being able to scrape table

--- helper.py
+++ helper.py
@@ -3,8 +3,9 @@ import numpy as np
 import requests
 from bs4 import BeautifulSoup
 import re
-from datetime import datetime
-from typing import Optional
+from datetime import datetime, timedelta
+from datetime import date
+from typing import Optional, List, Union, Tuple
 import dateparser
 import pytz
 
@@ -30,7 +31,23 @@ def get_harts_html(url: str) -> Optional[str]:
     return result
 
 
-def extract_table(url: str) -> Optional[pd.DataFrame]:
+def is_weekday(day: date) -> bool:
+    """
+    Returns the boolean flag if the date is a weekday
+
+    Parameters:
+        day (date): The current date
+
+    Returns:
+        bool: boolean flag if the day is a weekday
+    """
+    is_weekday = False
+    if day.weekday() in [0, 1, 2, 3, 4]:
+        is_weekday = True
+    return is_weekday
+
+
+def get_harts_html(url: str) -> Optional[str]:
     """
     Returns the HTML content of the URL.
 
@@ -38,34 +55,68 @@ def extract_table(url: str) -> Optional[pd.DataFrame]:
         url (str):The Harts Covid URL page
 
     Returns:
+        str: The page in HTML format
+    """
+
+    result = None
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            result = response.content
+    except:
+        pass
+    return result
+
+
+def scrape_table(url: str, daily: bool = True) -> Union[pd.DataFrame, List]:
+    """
+    Returns the HTML content of the URL.
+    Parameters:
+        url (str): The Harts Covid URL page
+        daily (bool): Tag to specify if results for the current page should be scraped
+    Returns:
         pd.DataFrame: A DataFrame of the scraped Table in the Covid Page
     """
     html_result = get_harts_html(url)
     df = None
-    if html_result:
-        soup = BeautifulSoup(html_result, features="lxml")
-        latest_link = soup.find("h2")
-        covid_details_list = list(latest_link.next_siblings)
-        start_date = covid_details_list[0]
-        covid_table = covid_details_list[1]
+    start_week = None
+    start_week_text = None
+    result = None
 
-        df = pd.read_html(str(covid_table))[0]
-        df = df.rename(columns={"Unnamed: 0": "days"})
-        british_tz = pytz.timezone("GMT")
+    british_tz = pytz.timezone("GMT")
+    time_now = datetime.now(tz=british_tz)
+    current_year = time_now.year
+    try:
+        if html_result:
+            soup = BeautifulSoup(html_result, features="lxml")
+            table_tag = soup.find("table")
+            table_html = str(table_tag)
 
-        time_now = datetime.now(tz=british_tz)
-        current_year = time_now.year
+            start_week_match = soup.find_all("h3", text=re.compile("Week commencing"))
+            if start_week_match:
+                start_week_text = start_week_match[0].text.lower()
+                month_match = r"(\d{0,2}\sjanuary|february|march|april|may|june|july|august|september|october|november|december)"
+                start_date_match = re.search(month_match, start_week_text)
 
-        start_datetime = dateparser.parse(
-            start_date.text.split("Week commencing")[-1] + str(current_year)
-        )
+                if start_date_match:
+                    start_week_text = (
+                        start_week_text[start_date_match.start() :]
+                        + ","
+                        + str(current_year)
+                    )
+                    start_week = dateparser.parse(start_week_text)
+            df = pd.read_html(table_html)[0]
+            df = df.rename(columns={"Unnamed: 0": "days"})
 
-        df["dates"] = pd.date_range(
-            start_datetime.strftime("%m/%d/%Y"), periods=df.shape[0]
-        )
+            df["dates"] = pd.date_range(
+                start_week.strftime("%m/%d/%Y"), periods=df.shape[0]
+            )
+            result = df
+        if daily:
+            result = df[df.dates == time_now.date().strftime("%Y-%m-%d")].to_dict(
+                orient="records"
+            )
 
-    return df
-
-
-def upload_db():
-    pass
+    except Exception as e:
+        print(e)
+    return result
