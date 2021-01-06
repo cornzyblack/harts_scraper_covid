01ca0a58a7276a55b7376ba339adbb75e0c79b66
Eniwoke Cornelius <eniwokepdm@gmail.com>
2021-01-06 20:25:48 +0100

    refactor: update functions

--- crud.py
+++ crud.py
@@ -37,15 +37,3 @@ def create_covid_test_result(
     db.commit()
     db.refresh(db_test_result)
     return db_test_result
-
-
-# def get_items(db: Session, skip: int = 0, limit: int = 100):
-#     return db.query(models.Item).offset(skip).limit(limit).all()
-
-
-# def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
-#     db_item = models.Item(**item.dict(), owner_id=user_id)
-#     db.add(db_item)
-#     db.commit()
-#     db.refresh(db_item)
-#     return db_item
