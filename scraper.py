41a402c455bfc652690da0874fe3d27ec4a0d803
Eniwoke Cornelius <eniwokepdm@gmail.com>
2021-02-10 01:40:26 +0000

    chore: remove unecessary file

--- scraper.py
+++
@@ -1,8 +0,0 @@
-from helper import scrape_table
-import pandas as pd
-
-url = "https://www.herts.ac.uk/coronavirus/covid-19-case-tracker"
-result = scrape_table(url)
-print(result)
-if isinstance(result, pd.DataFrame):
-    print(result.to_json(orient="records"))
